65f162774d984945126cd8476c89d780a3f2ddbb
nicksherma <62153054+nicksherma@users.noreply.github.com>
2025-07-02 17:17:15 +0800

    Delete files/etc/uci-defaults/99-enable-ssh.sh

--- files/etc/uci-defaults/99-enable-ssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-uci set dropbear.@dropbear[0].PasswordAuth='on'
-uci set dropbear.@dropbear[0].RootPasswordAuth='on'
-uci set dropbear.@dropbear[0].Port='22'
-uci commit dropbear
-/etc/init.d/dropbear enable
-/etc/init.d/dropbear start
-exit 0
