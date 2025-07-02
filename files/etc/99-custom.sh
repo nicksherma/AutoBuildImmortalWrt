53659b953ec3e5a52b3a692b1395c0ff343126cd
nicksherma <62153054+nicksherma@users.noreply.github.com>
2025-07-02 15:11:11 +0800

    Create 99-custom.sh

---
+++ files/etc/99-custom.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+uci set dropbear.@dropbear[0].PasswordAuth='on'
+uci set dropbear.@dropbear[0].RootPasswordAuth='on'
+uci set dropbear.@dropbear[0].Port='22'
+uci commit dropbear
+/etc/init.d/dropbear enable
+/etc/init.d/dropbear start
